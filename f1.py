a0975748cbe28d2d7496b9ddc898c7d3b932f029
Maneesh Kudulla <126312620+Maneesh0032@users.noreply.github.com>
2026-02-12 11:02:56 +0530

    Day1 codes in codegnan
    first day code commit , python intro

---
+++ f1.py
@@ -0,0 +1,9 @@
+a=5
+b=6
+c=a+b
+d="hi rey how r u!!"
+print(c)
+print(d,"\n")
+for i in range(1,a+1):
+    print(i," time in loop")
+print("\ntoday python class was very boring but little intresting")
