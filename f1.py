52503515d70cc288c8d03096eb08629811a63e3f
Maneesh Kudulla <126312620+Maneesh0032@users.noreply.github.com>
2026-02-12 19:37:19 +0530

    commit

--- f1.py
+++ f1.py
@@ -1,9 +1,8 @@
 a=5
 b=6
 c=a+b
-d="hi rey how r u!!"
+d="hi!"
 print(c)
 print(d,"\n")
-for i in range(1,a+1):
-    print(i," time in loop")
-print("\ntoday python class was very boring but little intresting")
+print("\ntoday python class was about , python introduction , features , and few sample programs")
+
