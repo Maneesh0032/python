47251d0293d80043c370ab4005d2ffdc7eac9ffc
Maneesh Kudulla <126312620+Maneesh0032@users.noreply.github.com>
2026-02-15 20:02:26 +0530

    commit
    Day 3

---
+++ f3.py
@@ -0,0 +1,82 @@
+"""
+
+# check whether the number entered is Zero 
+
+num=int(input())
+if num == 0:
+    print("It is a Zero")
+print("end of program ,it is done")
+
+"""
+
+"""
+
+# check whether the number entered is Zero or not.(using ==)
+
+num=int(input())
+if num == 0:
+    print("It is a Zero")
+else:
+    print("Its is not a Zero")
+print("end of program ,it is done")
+
+"""
+
+"""
+
+# check whether the number entered is Zero or not.(using !=)
+
+num=int(input())
+if num != 0:
+    print("It is  not a Zero")
+else:
+    print("Its is a Zero")
+print("end of program ,it is done")
+
+"""
+
+"""
+
+# find greater/bigger number among two numbers
+
+a = int(input())
+b = int(input())
+if a > b :
+    print(a ," is greater than ",b)
+else:print( b,"is greater than ",a)
+
+print("program end,its done")
+
+"""
+# Find weather number is even or not
+
+a = int(input())
+if a%2==0 :
+    print(a ," is even number")
+else:print( a," is Odd number")
+
+print("program end,its done")
+
+# find whether is divisible by 5 or not
+a = int(input())
+if a%5==0 :
+    print(a ," divisible by 5")
+else:print( a," is not divisible by 5")
+
+print("program end,its done")
+
+# find whether the number is divisible by both( 5 and 3 )or not
+
+# find whether the number is divisible by any one number between (4 or 3) or not
+
+
+
+
+
+
+
+
+
+
+
+
